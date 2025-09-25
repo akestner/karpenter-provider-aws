fc09c135307fe35278b99aa0a16039a0c92c3989
Alex Kestner <kestnera@amazon.com>
2025-09-25 16:40:46 -0700

    makes github script work across both repos

--- hack/github/feature_request_reactions.py
+++ hack/github/feature_request_reactions.py
@@ -11,8 +11,6 @@ from typing import Union
 from github import Github
 from github.Repository import Repository
 
-print('Getting popular feature requests...')
-
 # To create a GitHub token, see below (the token doesn't need to include any scopes):
 # https://help.github.com/en/github/authenticating-to-github/creating-a-personal-access-token-for-the-command-line
 github = Github(os.environ.get('GH_TOKEN'))
@@ -21,23 +19,36 @@ issue_reaction_count: list[dict[str, Union[int, str]]] = []
 PLUS_ONE_REACTION_STRINGS = ['+1', 'heart', 'hooray', 'rocket', 'eyes']
 ISSUE_LABELS = ['feature']
 
-repo: Repository = github.get_repo('aws/karpenter-provider-aws')
-open_issues = repo.get_issues(state='open', labels=ISSUE_LABELS)
-for issue in open_issues:
-  # count unique +1s
-  usernames: set[str] = set()
-  plus_ones = 0
-  for reaction in issue.get_reactions():
-    username = reaction.user.login
-    if reaction.content in PLUS_ONE_REACTION_STRINGS and username not in usernames:
-      usernames.add(reaction.user.login)
-      plus_ones += 1
+repo_details: list[dict[str, Union[Repository, str]]] = [
+  {
+    'repo': github.get_repo('aws/karpenter-provider-aws'),
+    'feature_label': 'feature'
+  },
+  {
+    'repo': github.get_repo('kubernetes-sigs/karpenter'),
+    'feature_label': 'kind/feature'
+  }
+]
 
-  issue_reaction_count.append({
-    'title': issue.title,
-    'url': issue.html_url,
-    'reactions': plus_ones
-  })
+for details in repo_details:
+  repo = details['repo']
+  feature_label = details['feature_label']
+  open_issues = repo.get_issues(state='open', labels=[feature_label])
+  for issue in open_issues:
+    # count unique +1s
+    usernames: set[str] = set()
+    plus_ones = 0
+    for reaction in issue.get_reactions():
+      username = reaction.user.login
+      if reaction.content in PLUS_ONE_REACTION_STRINGS and username not in usernames:
+        usernames.add(reaction.user.login)
+        plus_ones += 1
+
+    issue_reaction_count.append({
+      'title': issue.title,
+      'url': issue.html_url,
+      'reactions': plus_ones
+    })
 
 issue_row_list = [['Title', 'Url', 'Plus Ones']]
 for issue in sorted(issue_reaction_count, key=itemgetter('reactions'), reverse=True):
@@ -48,8 +59,6 @@ for issue in sorted(issue_reaction_count, key=itemgetter('reactions'), reverse=T
   ])
 
 # Write CSV data to STDOUT, redirect to file to persist, e.g.
-# ./hack/feature_request_reactions.py > "karpenter-feature-requests-$(date +"%Y-%m-%d").csv"
+# ./hack/github/feature_request_reactions.py > "karpenter-feature-requests-$(date +"%Y-%m-%d").csv"
 writer = csv.writer(sys.stdout)
 writer.writerows(issue_row_list)
-
-print('Done!')
